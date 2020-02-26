7ec5bc2304a19df44e5d1e5e1d5c0e63c1af13d8
Xin Sheng <xisheng@ucsd.edu>
2020-02-26 00:28:40 -0800

    improve layout

--- Renju.java
+++ Renju.java
@@ -8,59 +8,63 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.Random;
 
 public class Renju{
+	static String displayA = "  -> Player 1                 Player 2    ";
+	static String displayB = "     Player 1                 Player 2 <- ";
+	static char a = 'O';           // player 1
+	static char b = 'X';           // player 2
+	static int SIZE = 0;       	// size of board: 1-9
+	static int WINCOND = 0;        // number of pieces to win: 1-9
+	static int MODE = 0;           // 1 is single player mode, 2 is multiplayer mode
+	
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		int SIZE = 0;       	// size of board: 1-9
-		int WINCOND = 0;        // number of pieces to win: 1-9
-		int MODE = 0;           // 1 is single player mode, 2 is multiplayer mode
-		char a = 'O';           // player 1
-		char b = 'X';           // player 2
-		char win = null;
-		String displayA = "    -> Player 1                    Player 2    ";
-		String displayB = "       Player 1                    Player 2 <- ";
-		char round = "";
+		char win = '\0';
+		char round = '\0';
+		Renju obj = new Renju();
 
 		//prompt the player to choose mode
-		while(MODE != 1 && MODE != 2){
+		while(Renju.MODE != 1 && Renju.MODE != 2){
 			System.out.println("Choose mode:\n1: single player mode\n2: multiplayer mode");
-			MODE = in.nextInt();
+			Renju.MODE = in.nextInt();
 			in.nextLine();
 		}
 
-		while(SIZE < 1 || SIZE > 9){
+		while(Renju.SIZE < 1 || Renju.SIZE > 9){
 			System.out.println("Choose the size(length of size) of board: 1-9");
-			SIZE = in.nextInt();
+			Renju.SIZE = in.nextInt();
 			in.nextLine();
 		}
 
-		while(WINCOND < 1 || WINCOND > 9){
+		while(Renju.WINCOND < 1 || Renju.WINCOND > 9){
 			System.out.println("Choose the condition(number of pieces) to win: 1-9");
-			WINCOND = in.nextInt();
+			Renju.WINCOND = in.nextInt();
 			in.nextLine();
 		}
 
-		char board[][] = new char[SIZE+1][SIZE+1];
+		// create, initialize, and print the board
+		char board[][] = new char[Renju.SIZE+1][Renju.SIZE+1];
 		boardInit(board);
 		round = roundInit();
 
-		printBoard(board);
+		printBoard(board,round);
 
 
 	}
 
 	static void boardInit(char[][] board){
-		int row = 1;
-		int col = 0;
+		char row = '@';
+		int col = 1;
 		for(int i = 0; i < board.length; i++){
 			for(int j = 0; j < board[i].length; j++){
 				if(j == 0){
+					board[i][j] = row;
+					row = (char)(row+1);
+				}else if(i == 0){
 					board[i][j] = (char)(col+'0');
 					col++;
-				}else if(i == 0){
-					board[i][j] = (char)(row+'0');
-					row++;
 				}else{
 					board[i][j] = '-';
 				}
@@ -69,9 +73,14 @@ public class Renju{
 		board[0][0] = ' ';
 	}
 
-	static void printBoard(char[][] board){
+	static void printBoard(char[][] board, char round){
+		for(int i = 0; i < 3; i++){
+			System.out.println("");
+		}
+		printRound(round);
 		System.out.println("");
 		for(int i = 0; i < board.length; i++){
+			System.out.print("           ");
 			for(int j = 0; j < board[i].length; j++){
 				System.out.print(board[i][j] + " ");
 			}
@@ -79,13 +88,24 @@ public class Renju{
 		}
 	}
 
-	static int roundInit(){
+	static char roundInit(){
+		Random rand = new Random();
 		int i = rand.nextInt(1);
 		if(i == 0){
 			return 'A';
 		}else{
 			return 'B';
 		}
+	}
+
+	static void printRound(char round){
+		System.out.println("");
+		if(round == 'A'){
+			System.out.println(Renju.displayA);
+		}else if(round == 'B'){
+			System.out.println(Renju.displayB);
+		}
+		System.out.println("");
 	}
 		
 }
